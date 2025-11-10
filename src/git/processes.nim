e9cf722d7ad8b6fc1bc95fb63f3af7c8a1cdb94b
vorgestern <fisches@gmx.de>
2025-11-10 19:27:49 +0100

    follow: Verbesserung
    Die Abwicklung des git-Befehls wurde ebenso wie bei log und diff in git/processes.nim ausgelagert.
    In page/follow wird nur noch die Html-Formatierung abgewickelt.

--- src/git/processes.nim
+++ src/git/processes.nim
@@ -1,5 +1,5 @@
 
-import std/[osproc, strutils, streams]
+import std/[osproc, strutils, streams, times]
 import npeg
 
 type
@@ -226,3 +226,95 @@ proc gitstatus*(): tuple[status: RepoStatus, cmd: string] =
         for a in args: X=X & " " & a
         X
     (parse_status(Lines), cmd)
+
+# =====================================================================
+
+type
+    filestatus=tuple[status: FileStatus, path: string, oldpath: string]
+    Commit* =object
+        hash*: string
+        parents*: seq[string]
+        author*: string
+        date*: DateTime
+        subject*: string
+        details*: seq[string]
+        files*: seq[filestatus]
+
+proc parse_log(L: seq[string]): seq[Commit]=
+    type
+        context=enum None, Header, Subject, Details, Files
+        parsercontext=object
+            st: context
+            was: ptr seq[Commit]
+    const loglineparser=peg("line", e: parsercontext):
+        hash <- +{'0'..'9', 'a'..'f'}
+        path <- +{33..255}
+        commit_pp <- "commit " * >hash * @>hash * @>hash:
+            e.was[].add Commit(hash: substr($1, 0, 8), parents: @[substr($2, 0, 8), substr($3, 0, 8)])
+            e.st=Header
+        commit_p <- "commit " * >hash * @>hash:
+            e.was[].add Commit(hash: substr($1, 0, 8), parents: @[substr($2, 0, 8)])
+            e.st=Header
+        commit <- "commit " * >hash * !1:
+            e.was[].add Commit(hash: substr($1, 0, 8), parents: @["00000000"])
+            e.st=Header
+        authorname <- {33..128} * +{33..128}
+        author <- "Author:" * @>authorname * @'<':
+            e.was[^1].author= $1
+        datestring <- {'0'..'9', '-'}[10] * @{'0'..'9', ':'}[8] * @ {'0'..'9', '-', '+'}[5]
+        date <- "Date: " * @>datestring * !1:
+            let ts=substr($1, 0, 18)
+            e.was[^1].date=times.parse(ts, "yyyy-MM-dd HH:mm:ss")
+        empty <- *{' ', '\t'} * !1:
+            e.st=case e.st
+            of Header:  Subject
+            of Subject: Details
+            of Details: Details
+            else: Files
+        comment <- "    " * >+1:
+            case e.st
+            of Subject:
+                e.was[^1].subject= $1
+                e.st=Details
+            of Details: e.was[^1].details.add $1
+            else: discard
+        filestatus <- >{'A', 'M', 'D'} * +{' ','\t'} * >+1:
+            let stat=case $1
+            of "M": Modified
+            of "D": Deleted
+            of "A": Added
+            else: Other
+            e.was[^1].files.add (stat, $2, "")
+        filestatus_rename <- 'R' * {'0'..'9'}[3] * '\t' * >path * '\t' * >path:
+            e.was[^1].files.add (Renamed, $2, $1)
+        sonst <- >(*1) * !1:
+            echo "Nicht erwartet: ", $1
+        line <- commit_pp | commit_p | commit | author | date | empty | comment | filestatus | filestatus_rename | sonst
+    var e=parsercontext(st: None, was: addr result)
+    for z in L:
+        {.gcsafe.}:
+            if loglineparser.match(z, e).ok:
+                discard
+            else:
+                # error
+                # e.zeile="??????"
+                discard
+
+proc gitfollow*(path: string, num: int): tuple[result: seq[Commit], cmd: string]=
+    let args=block:
+        var X= @["log", "--follow", "--name-status", "--parents", "--date=iso-local"]
+        X.add if num>0: "-" & $num else: "-100"
+        X.add "--"
+        X.add path
+        X
+    let cmd=block:
+        var X="git"
+        for a in args: X=X & " " & a
+        X
+    let p=startprocess("git", args=args, options={poUsePath})
+    let pipe=outputstream(p)
+    var
+        Lines: seq[string]
+        line:  string
+    while readline(pipe, line): Lines.add line
+    (parse_log Lines, cmd)
--- src/page/follow.nim
+++ src/page/follow.nim
@@ -1,10 +1,11 @@
 
 import std/[tables, strformat]
-import std/[osproc, strutils, streams, times]
+import std/[strutils, times]
 import std/strtabs
 import npeg
 
 import mehr/helper
+import git/processes
 
 func htmlescape(s: string): string=replace(s, "<", "&lt;")
 
@@ -26,80 +27,8 @@ const html_template="""
 </html>
 """
 
-let TMonat={"01": "Jan", "02": "Feb", "03": "Mär", "04": "Apr", "05": "Mai", "06": "Jun",
+let TMonat {.used.}={"01": "Jan", "02": "Feb", "03": "Mär", "04": "Apr", "05": "Mai", "06": "Jun",
             "07": "Jul", "08": "Aug", "09": "Sep", "10": "Okt", "11": "Nov", "12": "Dez"}.newstringtable
-
-type
-    FileOp=enum Other, Modified, Deleted, Added, Renamed
-    filestatus=tuple[status: FileOp, path: string, oldpath: string]
-    Commit=object
-        hash: string
-        parents: seq[string]
-        author: string
-        date: DateTime
-        subject: string
-        details: seq[string]
-        files: seq[filestatus]
-
-proc parse_log(L: seq[string]): seq[Commit]=
-    type
-        context=enum None, Header, Subject, Details, Files
-        parsercontext=object
-            st: context
-            was: ptr seq[Commit]
-    const loglineparser=peg("line", e: parsercontext):
-        hash <- +{'0'..'9', 'a'..'f'}
-        path <- +{33..255}
-        commit_pp <- "commit " * >hash * @>hash * @>hash:
-            e.was[].add Commit(hash: substr($1, 0, 8), parents: @[substr($2, 0, 8), substr($3, 0, 8)])
-            e.st=Header
-        commit_p <- "commit " * >hash * @>hash:
-            e.was[].add Commit(hash: substr($1, 0, 8), parents: @[substr($2, 0, 8)])
-            e.st=Header
-        commit <- "commit " * >hash * !1:
-            e.was[].add Commit(hash: substr($1, 0, 8), parents: @["00000000"])
-            e.st=Header
-        authorname <- {33..128} * +{33..128}
-        author <- "Author:" * @>authorname * @'<':
-            e.was[^1].author= $1
-        datestring <- {'0'..'9', '-'}[10] * @{'0'..'9', ':'}[8] * @ {'0'..'9', '-', '+'}[5]
-        date <- "Date: " * @>datestring * !1:
-            let ts=substr($1, 0, 18)
-            e.was[^1].date=times.parse(ts, "yyyy-MM-dd HH:mm:ss")
-        empty <- *{' ', '\t'} * !1:
-            e.st=case e.st
-            of Header:  Subject
-            of Subject: Details
-            of Details: Details
-            else: Files
-        comment <- "    " * >+1:
-            case e.st
-            of Subject:
-                e.was[^1].subject= $1
-                e.st=Details
-            of Details: e.was[^1].details.add $1
-            else: discard
-        filestatus <- >{'A', 'M', 'D'} * +{' ','\t'} * >+1:
-            let stat=case $1
-            of "M": Modified
-            of "D": Deleted
-            of "A": Added
-            else: Other
-            e.was[^1].files.add (stat, $2, "")
-        filestatus_rename <- 'R' * {'0'..'9'}[3] * '\t' * >path * '\t' * >path:
-            e.was[^1].files.add (Renamed, $2, $1)
-        sonst <- >(*1) * !1:
-            echo "Nicht erwartet: ", $1
-        line <- commit_pp | commit_p | commit | author | date | empty | comment | filestatus | filestatus_rename | sonst
-    var e=parsercontext(st: None, was: addr result)
-    for z in L:
-        {.gcsafe.}:
-            if loglineparser.match(z, e).ok:
-                discard
-            else:
-                # error
-                # e.zeile="??????"
-                discard
 
 proc format_html(L: seq[Commit], highlight=""): string=
     result="<table class='diff'>"
@@ -125,38 +54,16 @@ proc format_html(L: seq[Commit], highlight=""): string=
     result.add "</table>"
 
 proc git_log_follow*(Args: Table[string,string]): string=
-
-    let gitargs=block:
-        var X= @["log", "--follow", "--name-status", "--parents", "--date=iso-local"]
-        if Args.contains "path":
-            if Args.contains "a":
-                var arg=Args["a"]   # &"^"
-                if Args.contains "b": arg.add ".."&Args["b"]
-                X.add arg
-            elif Args.contains "b":
-                X.add ".."&Args["b"]
-            if Args.contains "num": X.add "-" & Args["num"]
-            else: X.add "-100"
-            X.add "--"
-            X.add Args["path"]
-        X
-
-    # Starte git und sammele Ausgabezeilen ein.
-    let p=startprocess("git", args=gitargs, options={poUsePath})
-    let pipe=outputstream(p)
-    var
-        Loglines: seq[string]
-        line:  string
-    while readline(pipe, line): Loglines.add line
+    let
+        pathtofollow=Args.getordefault("path", "???")
+        num=parseint Args.getordefault("num", "100")
+        commithash=Args.getordefault("highlight", "")
+    echo "commithash=",commithash
+    let (L,cmd)=gitfollow(pathtofollow, num)
     let
         title="log_follow"
-        cmd=block:
-            var X="git"
-            for a in gitargs: X=X & " " & a
-            X
-        pathtofollow=Args.getordefault("path", "???")
         cssurl="/gitrelief.css"
-        content=format_html(parse_log Loglines, Args.getordefault("highlight", ""))
+        content=format_html(L, commithash)
     return fmt html_template
 
 # =====================================================================
