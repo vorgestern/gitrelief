f77769a96a52ca87608ccc945ee95c103964ab26
vorgestern <fisches@gmx.de>
2025-11-14 13:10:34 +0100

    processes: Vereinfachung
    - Log und Follow benutzen denselben Parser.

--- src/git/processes.nim
+++ src/git/processes.nim
@@ -241,68 +241,6 @@ proc gitstatus*(): tuple[status: RepoStatus, cmd: string] =
     (parse_status(Lines), cmd)
 
 # =====================================================================
-#               gitfollow
-
-proc parse_follow(L: seq[string]): seq[Commit]=
-    type
-        context=enum None, Header, Subject, Details, Files
-        parsercontext=object
-            st: context
-            was: ptr seq[Commit]
-    const lineparser=peg("line", e: parsercontext):
-        hash <- +{'0'..'9', 'a'..'f'}
-        path <- +{33..255}
-        commit <- "commit " * +@>hash:
-            var parents: seq[SecureHash]
-            for k in 2..<capture.len: parents.add parsesecurehash capture[k].s
-            e.was[].add Commit(hash: parsesecurehash $1, parents: parents)
-            e.st=Header
-        authorname <- {33..128} * +{33..128}
-        author <- "Author:" * @>authorname * @'<': e.was[^1].author= $1
-        datestring <- {'0'..'9', '-'}[10] * @{'0'..'9', ':'}[8] * @ {'0'..'9', '-', '+'}[5]
-        date <- "Date: " * @>datestring * !1:
-            let ts=substr($1, 0, 18)
-            e.was[^1].date=times.parse(ts, "yyyy-MM-dd HH:mm:ss")
-        empty <- *{' ', '\t'} * !1:
-            e.st=case e.st
-            of Header:  Subject
-            of Subject: Details
-            of Details: Details
-            else: Files
-        comment <- "    " * >+1:
-            case e.st
-            of Subject:
-                e.was[^1].subject= $1
-                e.st=Details
-            of Details: e.was[^1].details.add $1
-            else: discard
-        filestatus_added <- 'A' * +{' ','\t'} * >+1: e.was[^1].files.add CommittedOperation(status: Added, path: $1)
-        filestatus_modified <- 'M' * +{' ','\t'} * >+1: e.was[^1].files.add CommittedOperation(status: Modified, path: $1)
-        filestatus_deleted <- 'D' * +{' ','\t'} * >+1: e.was[^1].files.add CommittedOperation(status: Deleted, path: $1)
-        filestatus_renamed <- 'R' * >{'0'..'9'}[3] * @>path * @>path: e.was[^1].files.add CommittedOperation(status: Renamed, newpath: $3, oldpath: $2)
-        sonst <- >(*1) * !1:
-            echo "Nicht erwartet: ", $1
-        line <- commit | author | date | empty | comment | filestatus_added | filestatus_modified | filestatus_deleted | filestatus_renamed | sonst
-    var e=parsercontext(st: None, was: addr result)
-    for z in L:
-        {.gcsafe.}:
-            discard lineparser.match(z, e)
-
-proc gitfollow*(path: string, num: int): tuple[result: seq[Commit], cmd: string]=
-    let args=block:
-        var X= @["log", "--follow", "--name-status", "--parents", "--date=iso-local"]
-        X.add if num>0: "-" & $num else: "-100"
-        X.add "--"
-        X.add path
-        X
-    let cmd=block:
-        var X="git"
-        for a in args: X=X & " " & a
-        X
-    (parse_follow exec_path("git", args), cmd)
-
-# =====================================================================
-#               gitlog
 
 proc parse_log(L: seq[string]): seq[Commit]=
     type
@@ -352,6 +290,25 @@ proc parse_log(L: seq[string]): seq[Commit]=
     for z in L:
         {.gcsafe.}:
             discard lineparser.match(z, e)
+
+# =====================================================================
+#               gitfollow
+
+proc gitfollow*(path: string, num: int): tuple[result: seq[Commit], cmd: string]=
+    let args=block:
+        var X= @["log", "--follow", "--name-status", "--parents", "--date=iso-local"]
+        X.add if num>0: "-" & $num else: "-100"
+        X.add "--"
+        X.add path
+        X
+    let cmd=block:
+        var X="git"
+        for a in args: X=X & " " & a
+        X
+    (parse_log exec_path("git", args), cmd)
+
+# =====================================================================
+#               gitlog
 
 proc gitlog*(num: int): tuple[commits: seq[Commit], cmd: string]=
     let args=block:
