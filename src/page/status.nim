89b506099b8516f5cdc61178988b6620678a6e7f
vorgestern <fisches@gmx.de>
2025-11-17 21:37:11 +0100

    Seite Status: Verbesserung (Tabellenanordnung)

--- src/page/status.nim
+++ src/page/status.nim
@@ -14,7 +14,7 @@ proc walkpublicdir(dir: Path): string=
         let p=replace(string path, string dir1, "")
         result.add fmt"{'\n'}<tr><td></td><td><a href='{p}'>{p}</a></td></tr>"
 
-proc format_html(Status: RepoStatus): tuple[a,b,c: string]=
+proc format_html(Status: RepoStatus): tuple[a,b: string]=
     var Controlled="<h3>Staged</h3><table class='nolines'>"
     for index,entry in Status.staged:
         let diff="\n    <a href='" & url_diff(shanull, shanull, true, entry.path) & "'>diff</a>"
@@ -31,16 +31,16 @@ proc format_html(Status: RepoStatus): tuple[a,b,c: string]=
         Controlled.add "\n" & fmt"<tr><td>{entry.status}</td><td>{diff}{follow}{stage}</td><td>{entry.path}</td></tr>"
     Controlled.add "</table>"
 
-    var NotControlled="<table class='nolines'>"
+    var NotControlled="<h3>Parse Errors</h3><table class='nolines'>"
+    for index,entry in Status.unparsed: NotControlled.add fmt"<tr><td>{entry}</td></tr>"
+    NotControlled.add "</table>"
+    NotControlled.add "<h3>Not controlled</h3><table class='nolines'>"
     for index,entry in Status.notcontrolled:
         let stage="<a href='" & url_stage(entry) & "'>stage</a>"
         NotControlled.add "\n" & fmt"<tr><td>{entry}</td><td>{stage}</td></tr>"
     NotControlled.add "</table>"
 
-    var ParseError="<table class='nolines'>"
-    for index,entry in Status.unparsed: ParseError.add fmt"<tr><td>{entry}</td></tr>"
-    ParseError.add "</table></p>"
-    (Controlled,NotControlled,ParseError)
+    (Controlled,NotControlled)
 
 # =====================================================================
 
@@ -58,7 +58,8 @@ proc page_status*(Args: Table[string,string], publicdir: string): string=
             X
     let
         html_title= $servertitle & " status"
-        (html_controlled,html_notcontrolled,html_failedtoparse)=format_html(Status)
+        html_publicfiles="<table class='nolines'>" & walkpublicdir(Path publicdir) & "</table>"
+        (html_controlled,html_notcontrolled)=format_html(Status)
         html_remoteurls=block:
             var X=""
             for (name,urls) in pairs(R):
@@ -78,6 +79,5 @@ proc page_status*(Args: Table[string,string], publicdir: string): string=
                 X.add "</td>"
             X.add "</tr>"
             X
-        html_localfiles="<table class='nolines'>" & walkpublicdir(Path publicdir) & "</table>"
 
     return fmt html_template_status
