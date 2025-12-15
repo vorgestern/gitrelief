c7289f153cae008d45a0079bcc884c54f446f8de
vorgestern <fisches@gmx.de>
2025-12-16 00:19:09 +0100

    gtk3: Erweiterung

--- startergui/gtk3.nim
+++ startergui/gtk3.nim
@@ -122,6 +122,8 @@ proc gtk_window_set_default_size*(window: Window; width: cint; height: cint) {.i
 
 # =====================================================================
 
+proc gtk_widget_destroy*(widget: Widget) {.importc: "gtk_widget_destroy", libgtk.}
+
 proc gtk_widget_get_name*(X: Widget): cstring {.importc: "gtk_widget_get_name", libgtk.}
 proc gtk_widget_set_name*(X: Widget; name: cstring) {.importc: "gtk_widget_set_name", libgtk.}
 proc `name=`*(X: Widget; name: cstring) {.importc: "gtk_widget_set_name", libgtk.}
@@ -249,9 +251,11 @@ proc gtk_container_forall*(X: Container; cb: Callback; data: Gpointer) {.importc
 
 # GTK_IS...
 proc gtk_bin_get_type*(): GType {.importc: "gtk_bin_get_type", libgtk.}
+proc gtk_container_get_type*(): GType {.importc: "gtk_container_get_type", libgtk.}
 proc gtk_widget_get_type*(): GType {.importc: "gtk_widget_get_type", libgtk.}
 proc gtk_entry_get_type*(): GType {.importc: "gtk_entry_get_type", libgtk.}
 proc GTK_IS_BIN*(obj: Widget): Gboolean= g_type_check_instance_is_a(cast[GTypeInstance](obj), gtk_bin_get_type())
+proc GTK_IS_CONTAINER*(obj: Widget): Gboolean= g_type_check_instance_is_a(cast[GTypeInstance](obj), gtk_container_get_type())
 proc GTK_IS_WIDGET*(obj: Widget): Gboolean= g_type_check_instance_is_a(cast[GTypeInstance](obj), gtk_widget_get_type())
 proc GTK_IS_ENTRY*(obj: Widget): Gboolean= g_type_check_instance_is_a(cast[GTypeInstance](obj), gtk_entry_get_type())
 
